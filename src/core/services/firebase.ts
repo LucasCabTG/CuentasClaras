c79667d8bb80c66d1a58777b6c85dea6fb7c4970
Lucas Caballero <lucas.caballero@coyasistemas.com.ar>
2025-09-27 16:12:22 -0300

    env variables

--- src/core/services/firebase.ts
+++ src/core/services/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // TODO: Replace with your own Firebase project's configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9-yAIgudDnQkIrYw1CCPKyx9vTwCa4lE",
-  authDomain: "cuentasclarassftwr.firebaseapp.com",
-  projectId: "cuentasclarassftwr",
-  storageBucket: "cuentasclarassftwr.firebasestorage.app",
-  messagingSenderId: "640839755847",
-  appId: "1:640839755847:web:0695c0b71f4c52a2a16962"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
